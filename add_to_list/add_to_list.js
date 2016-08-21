4c95562a09feb2a3ba037bb71ad41d9f57348481
Michal <ndwz123@gmail.com>
2016-08-21 14:11:03 +0200

    add_to_list enhancment

--- add_to_list/add_to_list.js
+++ add_to_list/add_to_list.js
@@ -14,10 +14,12 @@
     },
     render: function(){
       $(this.el).append("<button id='add_item'>Add item to list</button>");
+      $(this.el).append("<h1>Total items: "+ this.itemsCounter +"</h1>");
       $(this.el).append("<ul class='items_list'></ul>");
     },
     addItem:function() {
        this.itemsCounter ++;
+       $("h1").text("Total items: "+ this.itemsCounter);
        $('.items_list').append("<li class='test_item_"+this.itemsCounter+"'>Test item_"+ this.itemsCounter +"</li>");
     }
   });
